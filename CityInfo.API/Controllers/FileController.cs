b9738d326eee5ea568ca1db76a7806663203dfa1
johnboeira <johnboeira@gmail.com>
2024-12-11 18:39:39 -0300

    feat : Adiciona endpoint que retorna um arquivo pdf

---
+++ CityInfo.API/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+            ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+    }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        var pathFile = "aspnet-core.pdf";
+
+        if (!System.IO.File.Exists(pathFile))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(pathFile, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathFile);
+        return File(bytes, contentType, Path.GetFileName(pathFile));
+    }
+}
