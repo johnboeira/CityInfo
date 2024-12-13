adb52903b2ab99cc13e6aa986101e8d1adeeba71
johnboeira <johnboeira@gmail.com>
2024-12-13 18:42:09 -0300

    feat: Adicionando validação no endpoint post

--- CityInfo.API/Models/PointOfInterestCreationDTO.cs
+++ CityInfo.API/Models/PointOfInterestCreationDTO.cs
@@ -1,7 +1,13 @@
-﻿namespace CityInfo.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models;
 
 public class PointOfInterestCreationDTO
 {
+    [Required(ErrorMessage = "Provide a name")]
+    [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+
+    [MaxLength(200)]
     public string? Description { get; set; }
 }
